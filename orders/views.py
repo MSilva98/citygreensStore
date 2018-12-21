1ee3eaac98bcf28ff2d54e9df41d5e159e5c2e3c
Miguel Dinis <miguel-dinis@hotmail.com>
2018-12-21 23:07:05 +0000

    farto

--- orders/views.py
+++ orders/views.py
@@ -29,12 +29,12 @@ def order_create(request):
 		form = OrderCreateForm()
 	return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
 
-@staff_member_required
+#@staff_member_required
 def admin_order_detail(request, order_id):
 	order = get_object_or_404(Order, id = order_id)
 	return render(request, 'admin/orders/order/detail.html', {'order': order})
 
-@staff_member_required
+#@staff_member_required
 def admin_order_pdf(request, order_id):
 	order = get_object_or_404(Order, id=order_id)
 	html = render_to_string('orders/order/pdf.html',{'order': order})
