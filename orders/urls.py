81e83450c28a7af3de9f127b9f58e3a39e0033ab
Miguel Dinis <miguel-dinis@hotmail.com>
2018-12-21 22:22:28 +0000

    cenas

--- orders/urls.py
+++ orders/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
 	url(r'^create/$', views.order_create, name='order_create'),
 	url(r'^admin/order/(?P<order_id>\d+)/$', views.admin_order_detail, name = 'admin_order_detail'),
 	url(r'^admin/order/(?P<order_id>\d+)/pdf/$',views.admin_order_pdf, name='admin_order_pdf'),
-	url(r'^admin/order/orders',views.go_orders,name='go_order'),
+	url(r'^admin/order/orders/$',views.go_orders,name='go_order'),
 ]
